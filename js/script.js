26d2ff581c739b43fae12abc1f34516664cd1777
Mateusdev7 <mateusdev1337@gmail.com>
2022-07-07 16:08:27 -0300

    Ações com JavaScript

--- js/script.js
+++ js/script.js
@@ -1,3 +1,18 @@
-if(window.SimpleAnime) {
-    new SimpleAnime();
+function initAnimarScroll() {
+    const sections = document.querySelectorAll(".js-scroll");
+    if(sections.length) {
+        const windowMetade = window.innerHeight * 0.6;
+        function animarScroll() {
+            sections.forEach((section) => {
+                const sectionTop = section.getBoundingClientRect().top;
+                const isSectionVisible = (sectionTop - windowMetade) < 0;
+                if(isSectionVisible) {
+                    section.classList.add('ativo');
+                }
+            })
+        }
+        animarScroll();
+        window.addEventListener('scroll', animarScroll);
+    }
 }
+initAnimarScroll();
